6bb3cede5920662725b285ca5068fe2f3a876a4f
Neko <44510611+necaticakaci@users.noreply.github.com>
2021-12-16 16:17:18 +0300

    Update BUYRUK_COZUCU.vhd

--- rtl/BUYRUK_COZUCU.vhd
+++ rtl/BUYRUK_COZUCU.vhd
@@ -6,13 +6,13 @@ use WORK.MANTA.ALL;
 
 entity BUYRUK_COZUCU is
 	port(	saat, reset		: in std_logic;
-			bc_oku			: in std_logic;
-			bc_giris		: in std_logic_vector(buyruk_genisligi-1 downto 0);
-			bc_sabit_cikis 	: out std_logic_vector(veri_genisligi-1 downto 0);
-			bc_adres_cikis	: out std_logic_vector(adres_genisligi-1 downto 0);
-			bc_kd0_cikis	: out std_logic_vector(3 downto 0);
-			bc_kd1_cikis	: out std_logic_vector(3 downto 0);
-			bc_buyruk		: out buyruk);
+		bc_oku			: in std_logic;
+		bc_giris		: in std_logic_vector(buyruk_genisligi-1 downto 0);
+		bc_sabit_cikis 		: out std_logic_vector(veri_genisligi-1 downto 0);
+		bc_adres_cikis		: out std_logic_vector(adres_genisligi-1 downto 0);
+		bc_kd0_cikis		: out std_logic_vector(3 downto 0);
+		bc_kd1_cikis		: out std_logic_vector(3 downto 0);
+		bc_buyruk		: out buyruk);
 			
 end BUYRUK_COZUCU;
 
@@ -24,7 +24,7 @@ bc_sabit_cikis 	<= buyruk_kaydedicisi(veri_genisligi-1 downto 0); -- Buyruktaki 
 bc_adres_cikis	<= buyruk_kaydedicisi(11 downto 2); -- Buyruktaki adres kismi aktariliyor.
 bc_kd0_cikis	<= buyruk_kaydedicisi(11 downto 8); -- Buyruktaki birinci kaydedici numarasi aktariliyor.
 bc_kd1_cikis	<= buyruk_kaydedicisi(7 downto 4); -- Buyruktaki ikinci kaydedici numarai aktariliyor.
-bc_buyruk 		<= coz(buyruk_kaydedicisi); -- Buyruk kodu cozuluyor.
+bc_buyruk 	<= coz(buyruk_kaydedicisi); -- Buyruk kodu cozuluyor.
 
 	process(saat, reset)
 	begin
