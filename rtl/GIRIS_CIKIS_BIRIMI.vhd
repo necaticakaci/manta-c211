7ecbe5933e075d885e1aa56c535add8d2ceb3a98
Neko <44510611+necaticakaci@users.noreply.github.com>
2021-12-16 16:34:35 +0300

    Update GIRIS_CIKIS_BIRIMI.vhd

--- rtl/GIRIS_CIKIS_BIRIMI.vhd
+++ rtl/GIRIS_CIKIS_BIRIMI.vhd
@@ -6,14 +6,14 @@ use WORK.MANTA.ALL;
 
 entity GIRIS_CIKIS_BIRIMI is
 	port(	saat				: in std_logic;
-			gcb_oku				: in std_logic;
-			gcb_yaz				: in std_logic;
-			gcb_ivedi			: in std_logic;
-			gcb_veri_giris		: in std_logic_vector(veri_genisligi-1 downto 0);
-			gcb_sabit_giris		: in std_logic_vector(veri_genisligi-1 downto 0);
-			gcb_port_giris		: in std_logic_vector(giris_port_genisligi-1 downto 0); 
-			gcb_veri_cikis		: out std_logic_vector(veri_genisligi-1 downto 0); 
-			gcb_port_cikis		: out std_logic_vector(cikis_port_genisligi-1 downto 0)); 
+		gcb_oku				: in std_logic;
+		gcb_yaz				: in std_logic;
+		gcb_ivedi			: in std_logic;
+		gcb_veri_giris			: in std_logic_vector(veri_genisligi-1 downto 0);
+		gcb_sabit_giris			: in std_logic_vector(veri_genisligi-1 downto 0);
+		gcb_port_giris			: in std_logic_vector(giris_port_genisligi-1 downto 0); 
+		gcb_veri_cikis			: out std_logic_vector(veri_genisligi-1 downto 0); 
+		gcb_port_cikis			: out std_logic_vector(cikis_port_genisligi-1 downto 0)); 
 
 end GIRIS_CIKIS_BIRIMI;
 
